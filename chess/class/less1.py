77e001b55985d2debfed7b81d9296cbeeedd45fc
EntertainmentLover <tipomipo5@gmail.com>
2024-09-26 14:32:25 +0600

    Solutions

--- chess/class/less1.py
+++ chess/class/less1.py
@@ -81,13 +81,13 @@
 
 
 
-def pyramid(n, r, ss, es):
+def hanoi_pyramid(n, r, ss, es):
     if n == 1:
         print(1, r, es)
     else:
-        pyramid(n - 1, r, es, ss)
+        hanoi_pyramid(n - 1, r, es, ss)
         print(n, r, es)
-        pyramid(n - 1, ss, r, es)
+        hanoi_pyramid(n - 1, ss, r, es)
 
 n = int(input('Disks: '))
-pyramid(n, 1, 2, 3)
+hanoi_pyramid(n, 1, 2, 3)
