c3391d46e662030e74fd94bff0c06f6a605cc795
EntertainmentLover <tipomipo5@gmail.com>
2025-02-19 14:01:29 +0600

    m

--- codeforce.py
+++ codeforce.py
@@ -143,30 +143,45 @@
 # print(0)
 
 
-def main():
-    n = int(input())
-    if n&1:
-        print(-1); return
-    g = [[] for _ in range(n+1)]
-    for _ in range(n-1):
-        u,v = map(int, input().split())
-        g[u].append(v); g[v].append(u)
-        s = [0]*(n+1); ans = 0; st = [(1, 0, 0)]
-        while st:
-            u, p, f = st.pop()
-            if f:
-                s[u] = 1
-                for v in g[u]:
-                    if v==p: continue
-                    s[u] += s[v]
-                if p and s[u]&1 == 0: ans += 1
-            else:
-                st.append((u, p, 1))
-                for v in g[u]:
-                    if v==p: continue
-                    st.append((v, u, 0))
-        print(ans)
+# def main():
+#     n = int(input())
+#     if n&1:
+#         print(-1); return
+#     g = [[] for _ in range(n+1)]
+#     for _ in range(n-1):
+#         u,v = map(int, input().split())
+#         g[u].append(v); g[v].append(u)
+#         s = [0]*(n+1); ans = 0; st = [(1, 0, 0)]
+#         while st:
+#             u, p, f = st.pop()
+#             if f:
+#                 s[u] = 1
+#                 for v in g[u]:
+#                     if v==p: continue
+#                     s[u] += s[v]
+#                 if p and s[u]&1 == 0: ans += 1
+#             else:
+#                 st.append((u, p, 1))
+#                 for v in g[u]:
+#                     if v==p: continue
+#                     st.append((v, u, 0))
+#         print(ans)
 
-if __name__== "__main__":
-    main()
+# if __name__== "__main__":
+#     main()
     
+
+
+def s():
+    t = int(input())
+    for _ in range(t):
+        x = int(input())
+        pos = False
+        for c in range(0,100):
+            if x >= c * 111 and (x - c * 111) % 11 == 0:
+                pos = True
+                break
+        print("YES" if pos else "NO") 
+
+if __name__ == "__main__":
+    s()  
